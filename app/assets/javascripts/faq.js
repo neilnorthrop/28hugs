0e278ca5d21ac69dc0e8437ddc1089576e08add8
Patrickmnts <patrickmnts@gmail.com>
2013-07-17 15:02:29 -0700

    fix images at deploy & javascript load issue

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
 
-  // $(".slidingDiv").hide();
-  // $(".show_hide").show();
-
   $('.faq a').on('click', function(){
     $(this).parents('li').children('p').toggleClass('hidden');
-    console.log('pass');
     return false;
   });
 });
